1fb4f4102be826dbb7e0d06ae7566175a1ef7204
John Mark Manuel <manueljm016@gmail.com>
2025-12-20 15:16:56 +0800

    updated logo on pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,4 @@
-import DigitizeboxLogo from '@/app/ui/digitizebox-logo';
+import DigitizeBoxLogoLetters from '@/app/ui/digitizebox-logo-letters';
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
 
@@ -6,9 +6,9 @@ export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <DigitizeboxLogo />
+        <div className="flex h-20 w-full items-start justify-end bg-blue-500 p-3 md:h-36">
+          <div className="w-16 text-white md:w-18">
+            <DigitizeBoxLogoLetters color='white' />
           </div>
         </div>
         <Suspense>
--- app/payment/[id]/page.tsx
+++ app/payment/[id]/page.tsx
@@ -1,4 +1,4 @@
-import DigitizeboxLogo from '@/app/ui/digitizebox-logo';
+import DigitizeBoxLogoLetters from '@/app/ui/digitizebox-logo-letters';
 import PaymentForm from '@/app/ui/payment/payment-form';
 import { Suspense } from 'react';
 
@@ -9,9 +9,9 @@ export default async function LoginPage(props: { params: Promise<{ id: string }>
   return (
     <main className="flex items-center justify-center py-14">
       <div className="relative mx-auto flex w-full max-w-[800px] flex-col space-y-2.5 p-4">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <DigitizeboxLogo />
+        <div className="flex h-20 w-full items-start justify-end bg-blue-500 p-3 md:h-36">
+          <div className="w-16 text-white md:w-18">
+            <DigitizeBoxLogoLetters color='white' />
           </div>
         </div>
         <Suspense>
