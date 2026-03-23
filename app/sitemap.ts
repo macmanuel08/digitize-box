3336a854cd870eac9714a300e82f090536f40ed6
John Mark Manuel <manueljm016@gmail.com>
2026-03-23 19:38:36 +0800

    appointment scheduling page added to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,23 +1,30 @@
 import { MetadataRoute } from 'next'
+const today = new Date();
 
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: 'https://digitizebox.com',
-            lastModified: new Date(),
+            lastModified: today,
             changeFrequency: 'weekly',
             priority: 1,
         },
         {
             url: 'https://digitizebox.com/website-development',
-            lastModified: new Date(),
+            lastModified: today,
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: 'https://digitizebox.com/appointment-scheduling',
+            lastModified: today,
             changeFrequency: 'monthly',
             priority: 0.9,
         },
         /*
         {
             url: 'https://digitizebox.com/blog',
-            lastModified: new Date(),
+            lastModified: today,
             changeFrequency: 'weekly',
             priority: 0.8,
         },
