c9dfa0b3f2ec36028df35c22fedb187f287da8d0
John Mark Manuel <manueljm016@gmail.com>
2026-09-05 09:06:42 +0800

    fixed login page's ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,8 +5,8 @@ import { Suspense } from 'react';
 
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
+    <main className="flex items-center justify-center">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 px-4 py-36">
         <div className="flex h-20 w-full items-start justify-end bg-blue-500 p-3 md:h-36">
           <div className="w-16 text-white md:w-18">
             <DigitizeBoxLogoLetters color='white' />
